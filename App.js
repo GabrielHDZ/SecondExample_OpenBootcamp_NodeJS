aa592ad1d495ac6d2952a4a7bac3261e00d69785
GabrielHDZ <gabrielhdz0102@gmail.com>
2022-07-08 17:10:15 -0500

    update data

---
+++ App.js
@@ -0,0 +1,9 @@
+
+
+(function main() {
+    let nombre = "Gabriel de Jesus";
+    let edad = 25;
+    let isDeveloper = true;
+    let birth = new Date("February 01 1997");
+    console.log(`Nombre: ${nombre} \nEdad: ${edad}\nfecha de nacimiento:${birth} \nEs desarrollador? ${isDeveloper? "si":"no"}`);
+})()
